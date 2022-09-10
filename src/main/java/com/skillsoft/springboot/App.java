da84c9933e4df8f3856dac7311cc85174bf885ec
Joeri Abbo <joeriabbo@hotmail.com>
2022-09-10 09:43:00 +0200

    Icttrainingen : Building a Spring Boot Application.

--- src/main/java/com/skillsoft/springboot/App.java
+++ src/main/java/com/skillsoft/springboot/App.java
@@ -6,8 +6,6 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootConfiguration
-@EnableAutoConfiguration
-@ComponentScan
 public class App {
     public static void main(String[] args) {
         SpringApplication.run(App.class, args);
