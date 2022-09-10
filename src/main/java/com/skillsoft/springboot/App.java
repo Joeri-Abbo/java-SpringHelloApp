2eb969e15c76dfc4a9bef00168868422130c50b0
Joeri Abbo <joeriabbo@hotmail.com>
2022-09-10 11:02:40 +0200

    Icttrainingen : Generating and Deploying an External WAR File.

--- src/main/java/com/skillsoft/springboot/App.java
+++ src/main/java/com/skillsoft/springboot/App.java
@@ -2,12 +2,17 @@ package com.skillsoft.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class App {
+public class App extends SpringBootServletInitializer {
 
-	public static void main(String[] args) {
-		SpringApplication.run(App.class, args);
-	}
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(App.class);
+    }
 
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
+    }
 }
